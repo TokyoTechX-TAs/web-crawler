a8eb2139777a5668a348ad0ef10adcf5b8fad10f
Zarina <zarina.rakhimberdina@gmail.com>
2018-04-19 16:19:19 +0900

    update simple_run.py

--- simple_run.py
+++ simple_run.py
@@ -1,14 +1,40 @@
 import os
+import re
+import csv
+import pandas as pd
+import urllib.parse
 
-file_name="links.txt"
 
-with open(file_name) as f:
-    content = f.readlines()
-links = [x.strip() for x in content] 
+def crawl(category):
 
-user=""
-passwd=""
+	file_name=category+".csv"
+	links=[]
 
-string="python edx_crawler.py -u " + user +"  -p "+passwd+" -url "
-for link in links:
-	os.system(string+" " +link)
+	df = pd.read_csv(file_name)
+	links = df.URL #you can also use df['column_name']
+	
+	user=""
+	passwd=""
+
+	string="python edx_crawler.py -u " + user +"  -p "+passwd+" -url "
+	for link in links:
+		link=link.strip('\n')
+		link=urllib.parse.unquote(link,encoding='utf-8', errors='replace')
+		query=string+link
+		print ("<"+query+">")
+		try:
+			os.system(query)
+		except Exception as e:
+			print(e)
+			continue			
+
+
+def main():
+	#categs=["Business & Management"]
+	categs=["Business & Management", "Computer Science", "Humanities"]
+	for c in categs:
+		crawl(c)
+
+
+if __name__== "__main__":
+	main()		
