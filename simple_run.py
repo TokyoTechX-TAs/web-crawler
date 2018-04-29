a6d9ffdb057287edb6e205f506864a6fe7a0e556
Zarina <zarina.rakhimberdina@gmail.com>
2018-04-29 18:26:06 +0900

    fix errors

--- simple_run.py
+++ simple_run.py
@@ -12,17 +12,18 @@ def crawl(category):
 
 	df = pd.read_csv(file_name)
 	links = df.URL #you can also use df['column_name']
-	
-	user="lordviscaria@gmail.com"
-	passwd="27nov2532"
-	#https://courses.edx.org/courses/course-v1:UPValenciaX+BSP101x+1T2018/course/
+	#print ("Number of courses in csv: ",len(links))
 
-	string="python edx_crawler.py -u " + user +"  -p "+passwd+" -url "
+	user=""
+	passwd=""
+
+	query="python edx_crawler.py -u " + user +"  -p "+passwd+" -url "
 	for link in links:
 		link=link.strip('\n')
 		link=urllib.parse.unquote(link,encoding='utf-8', errors='replace')
-		query=string+" "+link
-		print ("<"+query+">")
+		query=query+" "+link
+	
+	#print ("<"+query+">")	
 	try:
 		os.system(query)
 	except Exception as e:
@@ -32,8 +33,10 @@ def crawl(category):
 def main():
 	#categs=["Business & Management"]
 	#categs=["Business & Management", "Computer Science", "Humanities"]
-	categs=["Engineering", "Math", "Physics", "Social Sciences"]	
+
+	categs=["Engineering", "Math", "Physics", "Social Sciences"]
 	for c in categs:
+		print (c)
 		crawl(c)
 
 
